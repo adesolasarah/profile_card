83add0ac369016e152d993b4b891e4fa2e7fe04e
adesolasarah <sarahadesola37@gmail.com>
2025-01-29 13:43:17 -0800

    made a change

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-// function updateTime() {
-//     const utcTimeElement = document.getElementById("utc-time");
-//     const now = new Date();
-//     utcTimeElement.textContent = "UTC Time: " + now.toUTCString();
-// }
-// updateTime();
